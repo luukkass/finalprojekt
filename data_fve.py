61ba1e39428fd725bb73dc431bb2100a1768c370
Lukas Miksovsky <lmiksov@gmail.com>
2024-05-03 21:51:36 +0200

    nevim we move on

--- data_fve.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-dataraw = pd.read_csv("data\datacsvnaexport_final.csv", sep = ";")
-dataraw['update time'] = dataraw['update time'].str.rstrip('.')
-dataraw['timestamp'] = pd.to_datetime(dataraw['update time'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-dataraw["rounded_timestamp"] = dataraw["timestamp"].dt.round("5min")
-
-dataraw.to_csv("fve_final.csv")
